988634c736b1867aa0cfa9026819ce7b091cf89d
augustov58 <augustovalbuena@gmail.com>
2023-04-02 00:47:25 -0400

    Added Core contract files

---
+++ core-potatoswap/hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+  solidity: "0.5.16",
+};
+
+export default config;
