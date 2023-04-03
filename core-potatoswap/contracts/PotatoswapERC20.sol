da66ae8c90c3b1a15ff82cb1b8053ef30e9273b7
augustov58 <augustovalbuena@gmail.com>
2023-04-03 00:05:32 -0400

    Changed names to of Uniswap to Potatoswap on contracts

--- core-potatoswap/contracts/PotatoswapERC20.sol
+++ core-potatoswap/contracts/PotatoswapERC20.sol
@@ -1,9 +1,9 @@
 pragma solidity =0.5.16;
 
-import './interfaces/IUniswapV2ERC20.sol';
+import './interfaces/IPotatoswapERC20.sol';
 import './libraries/SafeMath.sol';
 
-contract UniswapV2ERC20 is IUniswapV2ERC20 {
+contract PotatoswapERC20 is IPotatoswapERC20 {
     using SafeMath for uint;
 
     string public constant name = 'Uniswap V2';
