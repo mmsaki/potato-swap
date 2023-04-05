95630085633a0bc923ad008c6a71b70e1c0eec07
meeky <98189596+mmsaki@users.noreply.github.com>
2023-04-05 01:02:08 -0500

    chore: adding address

---
+++ potatoswap-frontend/pages/utils/index.ts
@@ -0,0 +1,3 @@
+export const PotatoRouterV2 = `0x8a7284c259d6EC7Bd9d1658808d37Ffe557415A8`;
+export const PotatoTokenAddress = `0x1612d1706B7063525e29d742FE64be44a02DaDd6`;
+export const WETHAddress = `0x8a7284c259d6EC7Bd9d1658808d37Ffe557415A8`;
