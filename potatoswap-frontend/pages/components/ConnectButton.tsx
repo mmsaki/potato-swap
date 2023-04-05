19655f0bc2d65a31dad46a138daec5b1fb58309c
meeky <98189596+mmsaki@users.noreply.github.com>
2023-04-05 10:50:22 -0500

    fix: isconnected handled for green icon

--- potatoswap-frontend/pages/components/ConnectButton.tsx
+++ potatoswap-frontend/pages/components/ConnectButton.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { ConnectKitButton } from 'connectkit';
+import { useAccount } from 'wagmi';
 
 const ConnectButton = () => {
+	const { isConnected } = useAccount();
 	return (
 		<div className='relative'>
 			<ConnectKitButton />
-			<span className='top-2 left-10 absolute  w-3.5 h-3.5 bg-green-500 border-2 border-white dark:border-gray-800 rounded-full'></span>
+			{isConnected && (
+				<span className='top-2 left-10 absolute  w-3.5 h-3.5 bg-green-500 border-2 border-white dark:border-gray-800 rounded-full'></span>
+			)}
 		</div>
 	);
 };
