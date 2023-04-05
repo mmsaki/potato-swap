04c464a4391ac926b57631a5042f743af916cf27
meeky <98189596+mmsaki@users.noreply.github.com>
2023-04-05 07:20:43 -0500

    fix: image optimization and imports

--- potatoswap-frontend/next.config.js
+++ potatoswap-frontend/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**.githubusercontent.com',
+				hostname: '**.moralis.io',
+			},
+		],
+	},
 	webpack(config) {
 		// Grab the existing rule that handles SVG imports
 		const fileLoaderRule = config.module.rules.find((rule) =>
