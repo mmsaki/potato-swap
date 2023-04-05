45099ca0993076f1e165fb5c0d738a1de6ca479a
meeky <98189596+mmsaki@users.noreply.github.com>
2023-04-05 01:03:54 -0500

    feat: adding theme colors to tailwind config

--- potatoswap-frontend/tailwind.config.js
+++ potatoswap-frontend/tailwind.config.js
@@ -1,16 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
 
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				'light-gray': '#A0A0A0',
+				'dark-gray': '#646464',
+				'main-green': '#09BE9E',
+				'light-green': '#D7F0EC',
+				'dark-green': '#127364',
+			},
+		},
+	},
+	plugins: [],
+};
