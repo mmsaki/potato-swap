da698d872714b1c1a37aebc72e835e127f3bd5ca
meeky <98189596+mmsaki@users.noreply.github.com>
2023-04-05 00:55:37 -0500

    feat: connect kit integration

---
+++ potatoswap-frontend/pages/components/ConnectButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ConnectKitButton } from 'connectkit';
+
+const ConnectButton = () => {
+	return (
+		<>
+			<ConnectKitButton />
+		</>
+	);
+};
+
+export default ConnectButton;
